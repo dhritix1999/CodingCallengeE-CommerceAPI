48ce65e116a7b3197df533ba8c841fbc324111a1
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-06-29 23:01:04 +0400

    Repository: Added Comments

--- ecommerce/src/main/java/com/github/ecommerce/repository/DiscountRepository.java
+++ ecommerce/src/main/java/com/github/ecommerce/repository/DiscountRepository.java
@@ -3,9 +3,13 @@ package com.github.ecommerce.repository;
 import com.github.ecommerce.model.Discount;
 import com.github.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+/**
+ * Spring Data repository for the {@link Discount} entity.
+ */
+@Repository
 public interface DiscountRepository extends JpaRepository<Discount, String> {
 
 
--- ecommerce/src/main/java/com/github/ecommerce/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/github/ecommerce/repository/ProductRepository.java
@@ -5,6 +5,9 @@ import com.github.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Spring Data repository for the {@link Product} entity.
+ */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
