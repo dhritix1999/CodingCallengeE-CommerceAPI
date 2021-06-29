a87d9457b32d70e6fc5bb7a9da9ba73dd72306fd
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-06-29 23:01:18 +0400

    Service: Added Comments

--- ecommerce/src/main/java/com/github/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/github/ecommerce/service/ProductService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -23,6 +22,12 @@ public class ProductService {
         this.discountRepository = discountRepository;
     }
 
+    /**
+     * get the Total Price of the Products
+     *
+     * @param products the array of product Ids
+     * @return sum of the Products prices
+     */
     public Double getTotalBill(String[] products) {
 
         Map<String, Long> counts =
@@ -37,29 +42,38 @@ public class ProductService {
         return sum;
     }
 
-    public Double getProductPrice(String productId, Long productCount){
+
+    /**
+     * Calculate the price of each product
+     *
+     * @param productId        the id of product
+     * @param productQuantity the quantity of the product
+     * @return new Asset.
+     */
+    public Double getProductPrice(String productId, Long productQuantity) {
+//        Initialize product price
         Double productPriceSum = 0.0;
 
-        if (discountRepository.existsById(productId)){
+        if (discountRepository.existsById(productId)) {
             Discount discountOfP = discountRepository.getById(productId);
 
-            if (productCount >= discountOfP.getProductCount()){
+            if (productQuantity >= discountOfP.getProductCount()) {
 
                 //get number of times discount applies
-                int applicableForDiscountCount = Math.round((productCount / discountOfP.getProductCount()));
+                int applicableForDiscountCount = Math.round((productQuantity / discountOfP.getProductCount()));
 
                 //get total prices of discount items
                 productPriceSum += applicableForDiscountCount * discountOfP.getDiscountBundlePrice();
 
                 //remove the number of items accounted for discount
-                productCount -= applicableForDiscountCount * discountOfP.getProductCount();
+                productQuantity -= applicableForDiscountCount * discountOfP.getProductCount();
             }
             //apply regular pricing for the remaining product count
-            productPriceSum += discountOfP.getProduct().getUnitPrice() * productCount;
-        }
-        else{
+            productPriceSum += discountOfP.getProduct().getUnitPrice() * productQuantity;
+        } else {
+            //if product exists
             if (productRepository.existsById(productId)) {
-                productPriceSum+= productRepository.getById(productId).getUnitPrice() * productCount;
+                productPriceSum += productRepository.getById(productId).getUnitPrice() * productQuantity;
             }
         }
 
